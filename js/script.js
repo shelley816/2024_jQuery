928bf3676bb05f83b071158e9dd92403fec4c4eb
Shelley <jialing80916@gmail.com>
2024-09-03 13:01:00 +0800

    Traversing the DOM

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Traversing the DOM
+// next
+// $("#contact-methods").next().css({border: "2px solid red"});
+
+// prev
+// $("#social-nav").prev().css({border: "2px solid pink"});
+
+// parent
+// $(".banner-title").parents().css({border: "2px solid yellow"});
+
+// children
+// $("#social-nav").children().css({border: "2px solid green"});
+
+// find
+// $("#contact").find(".facebook").css({border: "2px solid purple"});
+
+// closest
+// $("#social-nav").closest(".wrapper").css({border: "2px solid orange"});
+
+
