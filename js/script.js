04079efd0978bf8f50c0be78e031093ce762e346
Shelley <jialing80916@gmail.com>
2024-09-05 12:43:00 +0800

    CSS with jQuery

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-// Removing Content
-$(".button").empty();
-$("#points-of-sale").empty();
-$("#contact img").remove();
+// CSS with jQuery
+// $("#social-nav").css("top", "-200px").css("left", "100px");
+
+$("#social-nav").css({
+    "top" : "-200px",
+    "left" : "100px",
+    "border" : "2px solid red"
+});
