0a96974192cae2bece8267b8b3c72257ac2bc912
Shelley <jialing80916@gmail.com>
2024-09-05 21:06:19 +0800

    The Event Object in jQuery

--- js/script.js
+++ js/script.js
@@ -1,18 +1,13 @@
-// Document Ready vs Window Load
+// The Event Object in jQuery
 
-// 第一個寫法
-$(document).on("ready", function(){
-    // do whatever you want to do
-})
-// 第二個寫法 (一的縮短版)
-$(function(){
-    // do whatever you want to do
-})
-// 第三個寫法 (等待所有網站載入完成，包含所有圖檔)
-$(window).on("load", function(){
-    // do whatever you want to do
-})
-// 第四個寫法 (三的縮短版吧...)
-$(window).load(function(){
-    // do whatever you want to do
+$(document).ready(function(){
+    
+    $("*").on("click", function(e){
+        console.log(e.target);
+        console.log("The event type is:" + e.type);
+        console.log("X co-ordinate of the event is:" + e.pageX);
+        console.log("Y co-ordinate of the event is:" + e.pageY);
+        e.stopPropagation();
+    })
+
 })
