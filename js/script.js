404eb3fbe00ae09e971b21c3c474b08d1f104ddf
Shelley <jialing80916@gmail.com>
2024-09-05 20:51:09 +0800

    Document Ready vs Window Load

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-// Event Helpers
+// Document Ready vs Window Load
 
-// $("#lead-banner").click(function(){
-//     alert("you clicked me");
-// })
-
-// $("#lead-banner").dblclick(function(){
-//     alert("you clicked me");
-// })
+// 第一個寫法
+$(document).on("ready", function(){
+    // do whatever you want to do
+})
+// 第二個寫法 (一的縮短版)
+$(function(){
+    // do whatever you want to do
+})
+// 第三個寫法 (等待所有網站載入完成，包含所有圖檔)
+$(window).on("load", function(){
+    // do whatever you want to do
+})
+// 第四個寫法 (三的縮短版吧...)
+$(window).load(function(){
+    // do whatever you want to do
+})
