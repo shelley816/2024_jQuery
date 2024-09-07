65601d41fd7a1d012219bb781bdd1c179c4b141a
Shelley <jialing80916@gmail.com>
2024-09-07 19:15:04 +0800

    Sliding Elements

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-// Hide and Show Elements
+// Sliding Elements
 
 $(document).ready(function(){
-    $("img[alt=map]").on("click", function(){
-        $("section > h2").toggle(1000);
+    $(".slide-button-up").on("click", function(){
+        $("#lead-banner").slideUp(1000);
+    })
+    $(".slide-button-down").on("click", function(){
+        $("#lead-banner").slideDown(2000, function(){
+            alert("ok");
+        });
     })
 })
