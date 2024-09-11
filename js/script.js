84ca48004bb6916af5dcccf10290dc29d1605460
Shelley <jialing80916@gmail.com>
2024-09-11 16:37:04 +0800

    Fading Animation Example

--- js/script.js
+++ js/script.js
@@ -5,15 +5,16 @@ $(document).ready(function(){
     let currentQuote = 0;
 
     function changeQuotes(){
-        $(allQuotes[currentQuote]).fadeOut(200);
+        $(allQuotes[currentQuote]).fadeOut(200, function(){
+            
+            if(currentQuote == allQuotes.length - 1){
+                currentQuote = 0;
+            } else {
+                currentQuote++;
+            }
+            $(allQuotes[currentQuote]).fadeIn(200);
 
-        if(currentQuote == allQuotes.length - 1){
-            currentQuote = 0;
-        } else {
-            currentQuote++;
-        }
-
-        $(allQuotes[currentQuote]).fadeIn(200);
+        });
     }
 
     const quoteTimer = setInterval(changeQuotes, 3000);
