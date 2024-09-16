6fe6828d929fb09746c8d95ba307f6c6a7772895
Shelley <jialing80916@gmail.com>
2024-09-16 15:20:15 +0800

    html css layout

---
+++ js/travelList.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    
+    let data = {
+        "types": [
+            "搭機必備物品", "衣物"
+        ],
+        "items": [
+            {
+                "type" : "搭機必備物品",
+                "Name" : "護照正本",
+                "Description" : ""
+            },
+            {
+                "type" : "搭機必備物品",
+                "Name" : "護照影本",
+                "Description" : "一張放隨身包、一張放行李箱"
+            }
+        ]
+    };
+
+
+
+});
