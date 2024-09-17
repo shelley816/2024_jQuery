7d1fffe3854ce802d72aeb7534074d01fcd67a6b
Shelley <jialing80916@gmail.com>
2024-09-17 13:44:28 +0800

    add css for layout, modify html, add some data of JS

--- js/travelList.js
+++ js/travelList.js
@@ -1,23 +1,87 @@
 $(document).ready(function(){
-    
     let data = {
         "types": [
-            "搭機必備物品", "衣物"
+            {
+                "name" : "搭機必備物品",
+                "description" : ""
+            },
+            {
+                "name" : "衣物",
+                "description" : ""
+            },
+            {
+                "name" : "3C 產品",
+                "description" : "電池要放隨身行李"
+            }
         ],
         "items": [
             {
                 "type" : "搭機必備物品",
-                "Name" : "護照正本",
-                "Description" : ""
+                "name" : "護照正本",
+                "state" : false,
+                "description" : ""
             },
             {
                 "type" : "搭機必備物品",
-                "Name" : "護照影本",
-                "Description" : "一張放隨身包、一張放行李箱"
+                "name" : "護照影本",
+                "state" : false,
+                "description" : "一張放隨身包、一張放行李箱"
+            },
+            {
+                "type" : "搭機必備物品",
+                "name" : "2寸大頭貼",
+                "state" : false,
+                "description" : "2張"
+            },
+            {
+                "type" : "搭機必備物品",
+                "name" : "外幣",
+                "state" : false,
+                "description" : "日幣換 xxx 元"
+            },
+            {
+                "type" : "搭機必備物品",
+                "name" : "信用卡",
+                "state" : false,
+                "description" : "準備2~3張，備好信用卡遺失聯絡電話"
+            },
+            {
+                "type" : "搭機必備物品",
+                "name" : "信用卡2",
+                "state" : false,
+                "description" : "一張存在 Apple pay 並放在家裡"
+            },
+            {
+                "type" : "衣物",
+                "name" : "衣、褲、裙",
+                "state" : false,
+                "description" : "6件衣服、3件褲子or裙子"
+            },
+            {
+                "type" : "衣物",
+                "name" : "內衣褲",
+                "state" : false,
+                "description" : "各5件"
+            },
+            {
+                "type" : "衣物",
+                "name" : "雪襪",
+                "state" : false,
+                "description" : "4雙"
             }
         ]
     };
 
+    // 初始
 
+    // 打勾後 state 改成 true, 回傳 data, 儲存 localstorage
+
+    // 新增物品
+
+    // 新增分類
+
+    // 編輯物品
+
+    // 刪除物品
 
 });
