78497af5f5044caa94a726c2293d346d0ffb1d3e
Shelley <jialing80916@gmail.com>
2024-09-19 15:47:16 +0800

    the results obtained asking for help from ChatGPT

--- js/travelList.js
+++ js/travelList.js
@@ -1,167 +1,169 @@
 $(document).ready(function(){
-    let data = {
-        "types": [
-            {
-                "name" : "搭機必備物品",
-                "description" : ""
-            },
-            {
-                "name" : "衣物",
-                "description" : ""
-            },
-            {
-                "name" : "3C 產品",
-                "description" : "電池要放隨身行李"
-            },
-            {
-                "name" : "生活類",
-                "description" : ""
-            }
-        ],
-        "items": [
-            {
-                "type" : "搭機必備物品",
-                "name" : "護照正本",
-                "state" : false,
-                "description" : ""
-            },
-            {
-                "type" : "搭機必備物品",
-                "name" : "護照影本",
-                "state" : false,
-                "description" : "一張放隨身包、一張放行李箱"
-            },
-            {
-                "type" : "搭機必備物品",
-                "name" : "2寸大頭貼",
-                "state" : false,
-                "description" : "2張"
-            },
-            {
-                "type" : "搭機必備物品",
-                "name" : "外幣",
-                "state" : false,
-                "description" : "日幣換 xxx 元"
-            },
-            {
-                "type" : "搭機必備物品",
-                "name" : "信用卡",
-                "state" : false,
-                "description" : "準備2~3張，備好信用卡遺失聯絡電話"
-            },
-            {
-                "type" : "搭機必備物品",
-                "name" : "信用卡2",
-                "state" : false,
-                "description" : "一張存在 Apple pay 並放在家裡"
-            },
-            {
-                "type" : "搭機必備物品",
-                "name" : "提款卡",
-                "state" : false,
-                "description" : "開啟跨國外幣提領功能"
-            },
-            {
-                "type" : "搭機必備物品",
-                "name" : "車票",
-                "state" : false,
-                "description" : ""
-            },
-            {
-                "type" : "搭機必備物品",
-                "name" : "景點票券",
-                "state" : false,
-                "description" : ""
-            },
-            {
-                "type" : "搭機必備物品",
-                "name" : "搭機小物",
-                "state" : false,
-                "description" : "眼罩、耳塞、頸枕、毛毯、閱讀刊物"
-            },
-            {
-                "type" : "搭機必備物品",
-                "name" : "隨身小物",
-                "state" : false,
-                "description" : "防盜腰包、密碼鎖、環保杯、環保餐具"
-            },
-            {
-                "type" : "搭機必備物品",
-                "name" : "行李秤",
-                "state" : false,
-                "description" : ""
-            },
-            {
-                "type" : "搭機必備物品",
-                "name" : "原子筆",
-                "state" : false,
-                "description" : "2支(寫入境表用)"
-            },
-            {
-                "type" : "搭機必備物品",
-                "name" : "網卡",
-                "state" : false,
-                "description" : "視個人需求(有買漫遊就不需要)"
-            },
-            {
-                "type" : "衣物",
-                "name" : "衣、褲、裙",
-                "state" : false,
-                "description" : "6件衣服、3件褲子or裙子"
-            },
-            {
-                "type" : "衣物",
-                "name" : "內衣褲",
-                "state" : false,
-                "description" : "各5件"
-            },
-            {
-                "type" : "衣物",
-                "name" : "雪襪",
-                "state" : false,
-                "description" : "4雙"
-            },
-            {
-                "type" : "衣物",
-                "name" : "鞋子、拖鞋",
-                "state" : false,
-                "description" : "1雙休閒鞋、1雙拖鞋"
-            },
-            {
-                "type" : "3C 產品",
-                "name" : "手機",
-                "state" : false,
-                "description" : "充電線、充電頭、手機掛繩"
-            },
-            {
-                "type" : "3C 產品",
-                "name" : "相機、360 相機",
-                "state" : false,
-                "description" : "記憶卡*2、電池*2、腳架、充電器"
-            },
-            {
-                "type" : "3C 產品",
-                "name" : "行動電源",
-                "state" : false,
-                "description" : ""
-            },
-            {
-                "type" : "3C 產品",
-                "name" : "萬國插座變壓器",
-                "state" : false,
-                "description" : ""
-            },
-            {
-                "type" : "生活類",
-                "name" : "盥洗用品",
-                "state" : false,
-                "description" : "牙刷、牙膏、牙線、牙籤、棉花棒、洗面乳"
-            }
-        ]
-    };
-    
-    let dataStr = JSON.stringify(data);
-    localStorage.setItem('travelList', dataStr);
+    // 初始資料寫入 localStorage（如果尚未儲存過）
+    if (!localStorage.getItem('travelList')) {
+        let data = {
+            "types": [
+                {
+                    "name" : "搭機必備物品",
+                    "description" : ""
+                },
+                {
+                    "name" : "衣物",
+                    "description" : ""
+                },
+                {
+                    "name" : "3C 產品",
+                    "description" : "電池要放隨身行李"
+                },
+                {
+                    "name" : "生活類",
+                    "description" : ""
+                }
+            ],
+            "items": [
+                {
+                    "type" : "搭機必備物品",
+                    "name" : "護照正本",
+                    "state" : false,
+                    "description" : ""
+                },
+                {
+                    "type" : "搭機必備物品",
+                    "name" : "護照影本",
+                    "state" : false,
+                    "description" : "一張放隨身包、一張放行李箱"
+                },
+                {
+                    "type" : "搭機必備物品",
+                    "name" : "2寸大頭貼",
+                    "state" : false,
+                    "description" : "2張"
+                },
+                {
+                    "type" : "搭機必備物品",
+                    "name" : "外幣",
+                    "state" : false,
+                    "description" : "日幣換 xxx 元"
+                },
+                {
+                    "type" : "搭機必備物品",
+                    "name" : "信用卡",
+                    "state" : false,
+                    "description" : "準備2~3張，備好信用卡遺失聯絡電話"
+                },
+                {
+                    "type" : "搭機必備物品",
+                    "name" : "信用卡2",
+                    "state" : false,
+                    "description" : "一張存在 Apple pay 並放在家裡"
+                },
+                {
+                    "type" : "搭機必備物品",
+                    "name" : "提款卡",
+                    "state" : false,
+                    "description" : "開啟跨國外幣提領功能"
+                },
+                {
+                    "type" : "搭機必備物品",
+                    "name" : "車票",
+                    "state" : false,
+                    "description" : ""
+                },
+                {
+                    "type" : "搭機必備物品",
+                    "name" : "景點票券",
+                    "state" : false,
+                    "description" : ""
+                },
+                {
+                    "type" : "搭機必備物品",
+                    "name" : "搭機小物",
+                    "state" : false,
+                    "description" : "眼罩、耳塞、頸枕、毛毯、閱讀刊物"
+                },
+                {
+                    "type" : "搭機必備物品",
+                    "name" : "隨身小物",
+                    "state" : false,
+                    "description" : "防盜腰包、密碼鎖、環保杯、環保餐具"
+                },
+                {
+                    "type" : "搭機必備物品",
+                    "name" : "行李秤",
+                    "state" : false,
+                    "description" : ""
+                },
+                {
+                    "type" : "搭機必備物品",
+                    "name" : "原子筆",
+                    "state" : false,
+                    "description" : "2支(寫入境表用)"
+                },
+                {
+                    "type" : "搭機必備物品",
+                    "name" : "網卡",
+                    "state" : false,
+                    "description" : "視個人需求(有買漫遊就不需要)"
+                },
+                {
+                    "type" : "衣物",
+                    "name" : "衣、褲、裙",
+                    "state" : false,
+                    "description" : "6件衣服、3件褲子or裙子"
+                },
+                {
+                    "type" : "衣物",
+                    "name" : "內衣褲",
+                    "state" : false,
+                    "description" : "各5件"
+                },
+                {
+                    "type" : "衣物",
+                    "name" : "雪襪",
+                    "state" : false,
+                    "description" : "4雙"
+                },
+                {
+                    "type" : "衣物",
+                    "name" : "鞋子、拖鞋",
+                    "state" : false,
+                    "description" : "1雙休閒鞋、1雙拖鞋"
+                },
+                {
+                    "type" : "3C 產品",
+                    "name" : "手機",
+                    "state" : false,
+                    "description" : "充電線、充電頭、手機掛繩"
+                },
+                {
+                    "type" : "3C 產品",
+                    "name" : "相機、360 相機",
+                    "state" : false,
+                    "description" : "記憶卡*2、電池*2、腳架、充電器"
+                },
+                {
+                    "type" : "3C 產品",
+                    "name" : "行動電源",
+                    "state" : false,
+                    "description" : ""
+                },
+                {
+                    "type" : "3C 產品",
+                    "name" : "萬國插座變壓器",
+                    "state" : false,
+                    "description" : ""
+                },
+                {
+                    "type" : "生活類",
+                    "name" : "盥洗用品",
+                    "state" : false,
+                    "description" : "牙刷、牙膏、牙線、牙籤、棉花棒、洗面乳"
+                }
+            ]
+        };
+        let dataStr = JSON.stringify(data);
+        localStorage.setItem('travelList', dataStr);
+    }
 
     let dataArr = JSON.parse(localStorage.getItem('travelList'));
     const container = $("#showContent");
@@ -203,29 +205,18 @@ $(document).ready(function(){
     };
     init();
 
-    // 打勾後 state 改成 true, 回傳 data, 儲存 localstorage
+    // 監聽 checkbox 變化並更新狀態
     const inputOj = $("input");
-    inputOj.on("click", function(e){
+    inputOj.on("change", function(e){
         const getNum = e.target.getAttribute("data-num");
         let currentState = e.target.checked;
         
-        data.items[getNum].state = currentState;
+        // 更新數據
+        dataArr.items[getNum].state = currentState;
         
-        dataStr = JSON.stringify(data);
+        // 儲存到 localStorage
+        dataStr = JSON.stringify(dataArr);
         localStorage.setItem('travelList', dataStr);
-        dataArr = JSON.parse(localStorage.getItem('travelList'));
-
-    })
-
-
-    // let test01 = data.items[1].state;
-    // $("input").prop("checked", test01);
-
-
-    // 新增物品
-
-    // 編輯物品
-
-    // 刪除物品
+    });
 
 });
