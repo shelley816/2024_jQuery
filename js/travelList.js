eba14c7d1e8777887879482e1423fde282acc547
Shelley <jialing80916@gmail.com>
2024-09-21 16:13:13 +0800

    modify some css

--- js/travelList.js
+++ js/travelList.js
@@ -8,12 +8,12 @@ $(document).ready(function(){
                     "description" : ""
                 },
                 {
-                    "name" : "衣物",
-                    "description" : ""
-                },
-                {
                     "name" : "3C 產品",
                     "description" : "電池要放隨身行李"
+                },
+                {
+                    "name" : "衣物",
+                    "description" : ""
                 },
                 {
                     "name" : "生活類",
@@ -25,199 +25,199 @@ $(document).ready(function(){
                     "type" : "搭機必備物品",
                     "name" : "護照正本",
                     "state" : false,
-                    "description" : ""
+                    "detail" : ""
                 },
                 {
                     "type" : "搭機必備物品",
                     "name" : "護照影本",
                     "state" : false,
-                    "description" : "一張放隨身包、一張放行李箱"
+                    "detail" : "一張放隨身包、一張放行李箱"
                 },
                 {
                     "type" : "搭機必備物品",
                     "name" : "2寸大頭貼",
                     "state" : false,
-                    "description" : "2張"
+                    "detail" : "2張"
                 },
                 {
                     "type" : "搭機必備物品",
                     "name" : "外幣",
                     "state" : false,
-                    "description" : "日幣換 xxx 元"
+                    "detail" : "日幣換 xxx 元"
                 },
                 {
                     "type" : "搭機必備物品",
                     "name" : "信用卡",
                     "state" : false,
-                    "description" : "準備2~3張，備好信用卡遺失聯絡電話"
+                    "detail" : "準備2~3張，備好信用卡遺失聯絡電話"
                 },
                 {
                     "type" : "搭機必備物品",
                     "name" : "信用卡2",
                     "state" : false,
-                    "description" : "一張存在 Apple pay 並放在家裡"
+                    "detail" : "一張存在 Apple pay 並放在家裡"
                 },
                 {
                     "type" : "搭機必備物品",
                     "name" : "提款卡",
                     "state" : false,
-                    "description" : "開啟跨國外幣提領功能"
+                    "detail" : "開啟跨國外幣提領功能"
                 },
                 {
                     "type" : "搭機必備物品",
                     "name" : "車票",
                     "state" : false,
-                    "description" : ""
+                    "detail" : ""
                 },
                 {
                     "type" : "搭機必備物品",
                     "name" : "景點票券",
                     "state" : false,
-                    "description" : ""
+                    "detail" : ""
                 },
                 {
                     "type" : "搭機必備物品",
                     "name" : "搭機小物",
                     "state" : false,
-                    "description" : "眼罩、耳塞、頸枕、毛毯、閱讀刊物"
+                    "detail" : "眼罩、耳塞、頸枕、毛毯、閱讀刊物"
                 },
                 {
                     "type" : "搭機必備物品",
                     "name" : "隨身小物",
                     "state" : false,
-                    "description" : "防盜腰包、密碼鎖、環保杯、環保餐具"
+                    "detail" : "防盜腰包、密碼鎖、環保杯、環保餐具"
                 },
                 {
                     "type" : "搭機必備物品",
                     "name" : "行李秤",
                     "state" : false,
-                    "description" : ""
+                    "detail" : ""
                 },
                 {
                     "type" : "搭機必備物品",
                     "name" : "原子筆",
                     "state" : false,
-                    "description" : "2支(寫入境表用)"
+                    "detail" : "2支(寫入境表用)"
                 },
                 {
                     "type" : "搭機必備物品",
                     "name" : "網卡",
                     "state" : false,
-                    "description" : "視個人需求(有買漫遊就不需要)"
+                    "detail" : "視個人需求(有買漫遊就不需要)"
                 },
                 {
                     "type" : "3C 產品",
                     "name" : "手機",
                     "state" : false,
-                    "description" : "充電線、充電頭、手機掛繩"
+                    "detail" : "充電線、充電頭、手機掛繩"
                 },
                 {
                     "type" : "3C 產品",
                     "name" : "相機、360 相機",
                     "state" : false,
-                    "description" : "記憶卡*2、電池*2、腳架、充電器"
+                    "detail" : "記憶卡*2、電池*2、腳架、充電器"
                 },
                 {
                     "type" : "3C 產品",
                     "name" : "行動電源",
                     "state" : false,
-                    "description" : ""
+                    "detail" : ""
                 },
                 {
                     "type" : "3C 產品",
                     "name" : "萬國插座變壓器",
                     "state" : false,
-                    "description" : ""
+                    "detail" : ""
                 },
                 {
                     "type" : "衣物",
                     "name" : "衣、褲、裙",
                     "state" : false,
-                    "description" : "6件衣服、3件褲子or裙子"
+                    "detail" : "6件衣服、3件褲子or裙子"
                 },
                 {
                     "type" : "衣物",
                     "name" : "內衣褲",
                     "state" : false,
-                    "description" : "各5件"
+                    "detail" : "各5件"
                 },
                 {
                     "type" : "衣物",
                     "name" : "雪襪",
                     "state" : false,
-                    "description" : "4雙"
+                    "detail" : "4雙"
                 },
                 {
                     "type" : "衣物",
                     "name" : "鞋子、拖鞋",
                     "state" : false,
-                    "description" : "1雙休閒鞋、1雙拖鞋"
+                    "detail" : "1雙休閒鞋、1雙拖鞋"
                 },
                 {
                     "type" : "衣物",
                     "name" : "外套",
                     "state" : false,
-                    "description" : "防曬薄外套、保暖厚外套"
+                    "detail" : "防曬薄外套、保暖厚外套"
                 },
                 {
                     "type" : "衣物",
                     "name" : "遮陽帽",
                     "state" : false,
-                    "description" : ""
+                    "detail" : ""
                 },
                 {
                     "type" : "生活類",
                     "name" : "盥洗用品",
                     "state" : false,
-                    "description" : "牙刷、牙膏、牙線、牙籤、棉花棒、洗面乳"
+                    "detail" : "牙刷、牙膏、牙線、牙籤、棉花棒、洗面乳"
                 },
                 {
                     "type" : "生活類",
                     "name" : "護理用品",
                     "state" : false,
-                    "description" : "消毒酒精、口罩、ok蹦、濕紙巾、衛生紙、防蚊液"
+                    "detail" : "消毒酒精、口罩、ok蹦、濕紙巾、衛生紙、防蚊液"
                 },
                 {
                     "type" : "生活類",
                     "name" : "防曬用品",
                     "state" : false,
-                    "description" : "太陽眼鏡、防曬乳"
+                    "detail" : "太陽眼鏡、防曬乳"
                 },
                 {
                     "type" : "生活類",
                     "name" : "個人藥品",
                     "state" : false,
-                    "description" : "暈車藥、感冒藥、B群、維他命C"
+                    "detail" : "暈車藥、感冒藥、B群、維他命C"
                 },
                 {
                     "type" : "生活類",
                     "name" : "保養品",
                     "state" : false,
-                    "description" : "化妝水、保濕、精華液、乳液"
+                    "detail" : "化妝水、保濕、精華液、乳液"
                 },
                 {
                     "type" : "生活類",
                     "name" : "卸妝",
                     "state" : false,
-                    "description" : "卸妝水、卸妝棉"
+                    "detail" : "卸妝水、卸妝棉"
                 },
                 {
                     "type" : "生活類",
                     "name" : "化妝",
                     "state" : false,
-                    "description" : "底妝、眼線、眼影、粉餅、口紅、遮瑕"
+                    "detail" : "底妝、眼線、眼影、粉餅、口紅、遮瑕"
                 },
                 {
                     "type" : "生活類",
                     "name" : "生理用品",
                     "state" : false,
-                    "description" : ""
+                    "detail" : ""
                 },
                 {
                     "type" : "生活類",
                     "name" : "其他",
                     "state" : false,
-                    "description" : "環保餐盒、膠帶"
+                    "detail" : "環保餐盒、膠帶"
                 }
             ]
         };
@@ -250,11 +250,11 @@ $(document).ready(function(){
         dataArr.items.forEach((item, i) => {
             let currentState =  dataArr.items[i].state;
             let itemHTML = `
-                <label class="pst-r col-12-xs col-6-sm col-4-lg col-3-xl" for="checkbox-nested-${i}">
+                <label class="pst-r col-12-xs col-6-sm col-4-lg col-3-xl ${currentState ? 'bg-primary-light-8' : ''}" for="checkbox-nested-${i}">
                     <input data-num=${i} type="checkbox" name="checkbox-nested-${i}" id="checkbox-nested-${i}" ${currentState ? 'checked' : ''}>
                     <span class="ml-1">${item.name}</span>
-                    <p class="text-gray">${item.description}</p>
-                    <a class="pst-a t-2 r-2" href="#"><img class="display-b" src="https://fakeimg.pl/21" alt=""></a>
+                    <p class="text-gray mt-1">${item.detail}</p>
+                    <a class="pst-a t-1 r-1" href="#"><img class="display-b" src="img/icon_travel_01.png" alt=""></a>
                 </label>
             `;
             let typeIndex = getTypes.indexOf(item.type);
@@ -265,15 +265,18 @@ $(document).ready(function(){
         });
     };
     init();
-
+    
     // 監聽 checkbox 變化並更新狀態（使用事件委派）
     $(document).on("change", "input[type=checkbox]", function(e){
         const getNum = e.target.getAttribute("data-num");
         let currentState = e.target.checked;
 
-        console.log(currentState);
-        // 更新數據
+        // 更新數據, UI
         dataArr.items[getNum].state = currentState;
+        $(this).parent().addClass("bg-primary-light-8");
+        if(!currentState){
+            $(this).parent().removeClass("bg-primary-light-8");
+        }
 
         // 儲存到 localStorage
         dataStr = JSON.stringify(dataArr);
@@ -337,7 +340,7 @@ $(document).ready(function(){
             "type" : typesVal,
             "name" : itemNameVal,
             "state" : false,
-            "description" : itemDetailVal
+            "detail" : itemDetailVal
         };
         dataArr.items.push(record);
         dataStr = JSON.stringify(dataArr);
